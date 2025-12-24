82e867beeac09a628606d85d5f9de81b71e846b3
Jelly Lee <liguodongiot@163.com>
2025-12-24 18:07:51 +0800

    update-2025-12-24

--- git-pull-push.sh
+++ git-pull-push.sh
@@ -2,8 +2,8 @@ git pull origin main
 git add .
 
 #time=`date -Iminutes`
-time=`date +"%Y-%m-%d_%H:%M:%S"`
-
+#time=`date +"%Y-%m-%d_%H:%M:%S"`
+time=`date +"%Y-%m-%d"`
 echo $time
 
 commit_info="update-""$time"
